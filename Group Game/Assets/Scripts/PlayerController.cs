afab228bfa16e6a561784992f59d76ee1f5e17f4
CampbellGray <69177775+CampbellGray@users.noreply.github.com>
2020-08-11 15:39:37 +0800

    changes some rooms

--- Group Game/Assets/Scripts/PlayerController.cs
+++ Group Game/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.XR;
 
 public class PlayerController : MonoBehaviour
 {
     // Created by David Borger
     // Based off this online tutorial https://www.youtube.com/watch?v=_QajrabyTJc&t= 
     public CharacterController controller;
+    public LevelBuilder levelBuilder;
 
     public float speed = 12f;
     public float gravity = -9;
@@ -18,6 +20,11 @@ public class PlayerController : MonoBehaviour
 
     //bool isGrounded;
     Vector3 velocity;
+
+    private void Start()
+    {
+        levelBuilder = FindObjectOfType<LevelBuilder>();
+    }
 
     /// <summary>
     /// This creates functionality for the player movement/jumping on the WASD and spacebar,
@@ -47,5 +54,13 @@ public class PlayerController : MonoBehaviour
         //velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Exit")
+        {
+            levelBuilder.GetComponent<LevelBuilder>().ResetLevelGenerator();
+        }
     }
 }
