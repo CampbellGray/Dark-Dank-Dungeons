d57dfb91bf9c6542d8598ebb05c04adee137a663
ThatOneGal <69178031+ThatOneGal@users.noreply.github.com>
2020-08-27 14:18:18 +0800

    added bullet is destroyed when it hits the walls.

--- Group Game/Assets/Scripts/ProjectileDamage.cs
+++ Group Game/Assets/Scripts/ProjectileDamage.cs
@@ -1,28 +1,38 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-////Kyana Bowers
+//Kyana Bowers
 
-//public class ProjectileDamage : MonoBehaviour
-//{
-//    //this script is attached to the projectile that the player fires at the enemy.
+public class ProjectileDamage : MonoBehaviour
+{
+    //this script is attached to the projectile that the player fires at the enemy.
+    //rooms must be tagged as "Room" to be destroyed when the bullet leaves.
 
-//    public float damageValue = 1f;
+    public float damageValue = 1f;
 
-//    /// <summary>
-//    /// When the enemy touches the projectile, enemy takes damage.
-//    /// </summary>
-//    /// <param name="other"></param>
-//    private void OnTriggerEnter(Collider other)
-//    {
-//        if (other.tag == "Enemy")
-//        {
-//            if (other.GetComponent<Health>().GetCurrentHealth() > 0)
-//            {
-//                other.GetComponent<Health>().ApplyDamage(damageValue);
-//            }
-//        }
-//    }
-//}
+    /// <summary>
+    /// When the enemy touches the projectile, enemy takes damage.
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            //if (other.GetComponent<Health>().GetCurrentHealth() > 0)
+            //{
+            //    other.GetComponent<Health>().ApplyDamage(damageValue);
+            //}
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Room")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
 
