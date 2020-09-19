1f3c3a3f86b98d0b074f24ab679f2171ad773195
CampbellGray <69177775+CampbellGray@users.noreply.github.com>
2020-09-19 15:40:36 +0800

    obby interactions

---
+++ Group Game/Assets/Scripts/Player/ObbyInteraction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObbyInteraction : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                if(Score.scoreValue >= 500)
+                {
+                    UI.currentHealth += 1;
+                    Score.scoreValue -= 500;
+                }
+            }
+        }
+    }
+}
---
+++ Group Game/Assets/Scripts/Player/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Wall")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+            Score.scoreValue += 50;
+        }
+    }
+}
