4e3fb6174f3429031778f4645dc9d5ff87119f20
ThatOneGal <69178031+ThatOneGal@users.noreply.github.com>
2020-11-19 09:46:27 +0800

    Fixed exit

--- Group Game/Assets/Scripts/Misc/Exit.cs
+++ Group Game/Assets/Scripts/Misc/Exit.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Kyana Bowers
+    // Dark Dank Dungeons
+
+    private int currentSceneNumber;
+
+    /// <summary>
+    /// Sets the current scene number to the active scene's build index.
+    /// </summary>
+    private void Start()
     {
-        
+        currentSceneNumber = SceneManager.GetActiveScene().buildIndex;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// If the player touches the exit portal, it checks to see if the active scene
+    /// is not the Lava level, and if it is, it loops back to the Sewer level.
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (other.gameObject.tag == "Player")
+        {
+            if (currentSceneNumber == 4)
+            {
+                SceneManager.LoadScene(1);
+            }
+            else
+            {
+                SceneManager.LoadScene(currentSceneNumber + 1);
+            }
+        }
     }
 }
+
