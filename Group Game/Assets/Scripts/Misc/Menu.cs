588ef4e7c7419cb0131e6eb50f23605ee4a55917
Sethanon5b <69177813+Sethanon5b@users.noreply.github.com>
2020-11-24 11:47:05 +0800

    Music slider added
    Music slider has been added. Sound clips have been mixed properly.

--- Group Game/Assets/Scripts/Misc/Menu.cs
+++ Group Game/Assets/Scripts/Misc/Menu.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Menu : MonoBehaviour
 {
     public bool manuState = true;
     public GameObject mainMenu;
     public GameObject leaderboard;
+    public AudioMixer audioMixer;
     private void Update()
     {
         if(manuState == true)
@@ -29,5 +31,10 @@ public class Menu : MonoBehaviour
     public void manuBoolFalse()
     {
         manuState = false;
+    }
+
+    public void SetVolume(float volume) 
+    {
+        audioMixer.SetFloat("volume", volume);
     }
 }
